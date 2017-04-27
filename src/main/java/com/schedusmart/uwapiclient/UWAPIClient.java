64785f31a6cea7a7d06114de066213f9f59a86c2
Sebastien <SSt.Denis25@gmail.com>
2017-04-27 18:50:06 -0400

    add code to automatically get relevant terms for the search menu

--- src/main/java/com/schedusmart/uwapiclient/UWAPIClient.java
+++ src/main/java/com/schedusmart/uwapiclient/UWAPIClient.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.ListIterator;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.Calendar;
 
 import com.google.gson.Gson;
 import com.schedusmart.schedulebuilder.Block;
@@ -135,7 +136,22 @@ public class UWAPIClient {
 	// getTerms returns term information from the UWaterloo API for each term with course
 	//    info available one the API.  The information is returned as an array of Term objects
 	public static Term[] getTerms() throws UWAPIException {
-		String[] termCodes = {"1171", "1175"}; //TODO: generate valid term codes automatically
+		int month = Calendar.getInstance().get(Calendar.MONTH);
+		int year = Calendar.getInstance().get(Calendar.YEAR) % 100; // last two digits
+				
+		String[] termCodes = new String[2];
+		
+		if (month <= Calendar.APRIL) {
+			termCodes[0] = "1" + year + "1";
+			termCodes[1] = "1" + year + "5";
+		} else if (month <= Calendar.AUGUST) {
+			termCodes[0] = "1" + year + "5";
+			termCodes[1] = "1" + year + "9";
+		} else {
+			termCodes[0] = "1" + year + "9";
+			termCodes[1] = "1" + (year + 1) + "1";
+		}
+		
 		ArrayList<Term> terms = new ArrayList<Term>();
 		
 		for (int i = 0; i < termCodes.length; ++i) {
